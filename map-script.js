1c6b9a64de47aef2e322ab2703b5d0f4a1a70f0b
Gayoung-Han <230981709+Gayoung-Han@users.noreply.github.com>
2025-11-11 17:28:01 +0100

    adjusted some css and shit

--- map-script.js
+++ map-script.js
@@ -2,9 +2,9 @@
 const mapPinsData = [
   { id: 1, top: "35%", left: "60%", title: "Main Runway Hall", desc: "Nørregade 14, 1165 København K", img: "/map/runway.png" },
   { id: 2, top: "65%", left: "30%", title: "TikTok Creator Hub", desc: "Vesterbrogade 72, 1620 København", img: "/map/tiktok.png" },
-  { id: 3, top: "55%", left: "80%", title: "Exhibition", desc: "Gothersgade 105, 1123 København K", img: "/map/exhibition.png" },
-  { id: 4, top: "25%", left: "75%", title: "Food Lounge", desc: "Læderstræde 18, 1201 København K", img: "/map/food.png" },
-  { id: 5, top: "35%", left: "20%", title: "CIFF", desc: "Studiestræde 45, 1554 København", img: "/map/ciff.png" }
+  { id: 3, top: "85%", left: "80%", title: "Exhibition", desc: "Gothersgade 105, 1123 København K", img: "/map/exhibition.png" },
+  { id: 4, top: "45%", left: "85%", title: "Food Lounge", desc: "Læderstræde 18, 1201 København K", img: "/map/food.png" },
+  { id: 5, top: "27%", left: "20%", title: "CIFF", desc: "Studiestræde 45, 1554 København", img: "/map/ciff.png" }
 ];
 
 // URL 파라미터 확인 (예: ?pin=5)
